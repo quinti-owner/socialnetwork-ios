62b0e5f46f95684cf73cbe4edf1d47db6b8eda54
quinti <quinti.owner@gmail.com>
2022-09-17 20:08:05 +0900

    プロジェクトを追加

---
+++ SocialNetwork/SocialNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by 古堅ニノスカ on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
